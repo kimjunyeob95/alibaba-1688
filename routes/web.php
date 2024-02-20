1c18f2dd14cab5b92b7bb297a7c43985808277d8
junyeob kim <junyeob.kim@onch3.co.kr>
2024-02-20 18:52:34 +0900

    feat: 초기 설계

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    abort(404);
 });
+
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
