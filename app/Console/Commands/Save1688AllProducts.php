efd22fe2a8f953be6de06ff3d74141587a49f2de
junyeob kim <junyeob.kim@onch3.co.kr>
2024-03-05 11:22:26 +0900

    feat: Pid 삭제 로직 수정

--- app/Console/Commands/Save1688AllProducts.php
+++ app/Console/Commands/Save1688AllProducts.php
@@ -7,7 +7,7 @@ use Symfony\Component\Process\Process;
 
 class Save1688AllProducts extends Command
 {
-    protected $signature   = 'save_1688_all_product';
+    protected $signature   = 'save_1688_all_product {--killpid=}';
     protected $description = '1688 모든 카테고리 상품 수집';
 
     public function __construct()
@@ -17,25 +17,24 @@ class Save1688AllProducts extends Command
     /*
      * 실행 구문
      * 모든 카테고리에 대해서 병렬적으로 프로세스 실행
-     * php artisan save_1688_all_product
+     * php artisan save_1688_all_product --killpid=false
     */
     public function handle()
     {
+        $killpid = $this->option('killpid', 'false');
+
         $getCategoryObjs = Category::get();
         $processes = [];
         foreach ($getCategoryObjs as $getCategoryObj) {
             $categoryId = $getCategoryObj->category_id;
-            $command    = "php artisan save_1688_product --categoryid={$categoryId}";
+            $command    = "php artisan save_1688_product --categoryid=" . escapeshellarg($categoryId);
 
             // 1. 실행 중인 동일 커맨드 찾은 후 PID들에 대해 강제 종료 실행
             $findProcessCommand = "ps aux | grep '".escapeshellcmd($command)."' | grep -v grep | awk '{print $2}'";
             $output = [];
             exec($findProcessCommand, $output);
-            foreach ($output as $pid) {
-                if (is_numeric($pid)) {
-                    echo "Killing process(categoryId {$categoryId}) with PID: $pid\n";
-                    exec("kill -9 $pid");
-                }
+            if($killpid === "true"){
+                $this->killExistingProcesses($output, $categoryId);
             }
 
             // 2. 커맨드 실행
@@ -43,9 +42,27 @@ class Save1688AllProducts extends Command
             $process->start();
             $processes[] = $process;
         }
-
         foreach ($processes as $process) {
             $process->wait();
+        }
+    }
+
+    
+    /**
+     * @func killExistingProcesses
+     * @description 'PID들에 대해 강제 종료 실행'
+     * @param array $pids
+     * @param $categoryId
+     */
+    protected function killExistingProcesses(array $pids, $categoryId)
+    {
+        foreach ($pids as $pid) {
+            if (is_numeric($pid)) {
+                echo "Gracefully terminating process(categoryId {$categoryId}) with PID: $pid\n";
+                exec("kill $pid"); // SIGTERM 신호를 보냄
+                sleep(1); // 프로세스가 종료될 시간을 줌
+                exec("kill -9 $pid"); // 여전히 실행 중인 경우 강제 종료
+            }
         }
     }
 }
