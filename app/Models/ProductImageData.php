0c5c08ae1048af6409b89af9b5906f5f48d8bfef
junyeob kim <junyeob.kim@onch3.co.kr>
2024-03-08 11:38:05 +0900

    feat: model softdelete 조건 추가

--- app/Models/ProductImageData.php
+++ app/Models/ProductImageData.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductImageData extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table      = 'product_image_datas';
     protected $guarded    = [];
