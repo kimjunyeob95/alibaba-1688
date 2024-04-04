6675c7b87877115f52c305a5eced049aa30d5e1e
mindabin <mindabin94@gmail.com>
2024-04-04 10:42:45 +0900

    feat : 이지셀 상품현황메뉴

--- resources/views/dashboard/shared/sidebar.blade.php
+++ resources/views/dashboard/shared/sidebar.blade.php
@@ -52,6 +52,22 @@
                 </li>
             </ul>
         </li>
+        <li class="nav-title">이지셀</li>
+        <li class="nav-group">
+            <a class="nav-link nav-group-toggle" href="javascript:;">
+                <svg class="nav-icon">
+                    <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-3d"></use>
+                </svg>
+                상품관리
+            </a>
+            <ul class="nav-group-items">
+                <li class="nav-item">
+                    <a class="nav-link" href="/easySell/product/list">
+                        상품 현황
+                    </a>
+                </li>
+            </ul>
+        </li>
     </ul>
     <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
 </div>
