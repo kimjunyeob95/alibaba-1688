50337652f57d9c0bdaf2fb3ac16fabfde7e20ef1
mindabin <mindabin94@gmail.com>
2024-04-18 09:48:01 +0900

    feat : 이지셀 상품등록 수정

--- app/Packages/EasySell.php
+++ app/Packages/EasySell.php
@@ -4,6 +4,7 @@ namespace App\Packages;
 
 use App\Abstracts\MallApiAbstract;
 use App\Constants\EasySellConstant;
+use App\Constants\ImageConstant;
 use App\Constants\MallConstant;
 use App\Constants\MallErrorMessageConstant;
 use App\Constants\ProductConstant;
@@ -262,7 +263,7 @@ class EasySell extends MallApiAbstract
         try {
             DB::beginTransaction();
 
-            $categoryObj = WCategory::select(["category_id","mapping_code"])->where("category_id", "!=", 0)->get();
+            $categoryObj = CategoryMapping::where("mapping_channel", ProductConstant::MAPPING_WAPP)->get();
             $categoryArr = $categoryObj->pluck("mapping_code","category_id")->toArray();
 
             $easysellCategory = OnchCategoryExcelDataCopy2::select("codenum","sellerhub_cate")
@@ -368,7 +369,7 @@ class EasySell extends MallApiAbstract
                 $unitInfo .= "{$optionNm}^^{$stock}^^{$setPrice}^^{$setPrice}^^{$option->option_price}::{$option->id}";
             }
 
-            $itemImage = implode("|", array_filter($prdObj->images->whereIn("img_type",["main","sub"])->pluck("img_url_trans")->toArray()));
+            $itemImage = implode("|", array_filter($prdObj->images->whereIn("img_type",["main","sub"])->where("is_except",ImageConstant::IS_EXCEPT_N)->pluck("img_url_trans")->toArray()));
 
             $voParams = [
                 "ItemNo"                => $offerId,
