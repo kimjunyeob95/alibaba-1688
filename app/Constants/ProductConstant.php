e84a1048e784d62207f53099c3b5d6b57384a98a
junyeob kim <junyeob.kim@onch3.co.kr>
2024-04-30 12:50:21 +0900

    feat: 옵션 수집 로직 추가

--- app/Constants/ProductConstant.php
+++ app/Constants/ProductConstant.php
@@ -9,11 +9,13 @@ class ProductConstant
     public const PRD_STATUS_PUBLISH = "published";
     public const PRD_STATUS_STOP    = "stop";
     public const PRD_STATUS_EXCEPT  = "except";
+    public const PRD_STATUS_MISS    = "miss";
 
     public const PRD_STATUS = [
         self::PRD_STATUS_PUBLISH => "정상판매",
         self::PRD_STATUS_STOP    => "판매중지",
         self::PRD_STATUS_EXCEPT  => "판매제외",
+        self::PRD_STATUS_MISS    => "정보누락",
     ];
 
     /** 전송상태 */
