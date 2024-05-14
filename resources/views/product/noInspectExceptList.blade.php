21601125848929858814a0da181e8bf10ae50c69
junyeob kim <junyeob.kim@onch3.co.kr>
2024-05-14 11:38:14 +0900

    feat: 검수중 페이지 수정

--- resources/views/product/noInspectExceptList.blade.php
+++ resources/views/product/noInspectExceptList.blade.php
@@ -349,15 +349,13 @@
                                     </td>
                                     <td class="text-center">
                                         @if ($data->img_inspect == null || $data->img_inspect->is_inspect == InspectConstant::IS_INSPECT_N)
-                                            <span class="text-danger">{{ InspectConstant::INSPECT_LIST[InspectConstant::INSPECT_IMAGE] }}</span>
+                                            <label class="text-danger">{{ InspectConstant::INSPECT_LIST[InspectConstant::INSPECT_IMAGE] }}</label>
                                         @endif
                                         @if ($data->prd_inspect == null || $data->prd_inspect->is_inspect == InspectConstant::IS_INSPECT_N)
-                                            <br>
-                                            <span class="text-danger">{{ InspectConstant::INSPECT_LIST[InspectConstant::INSPECT_PRODUCT] }}</span>
+                                            <label class="text-danger">{{ InspectConstant::INSPECT_LIST[InspectConstant::INSPECT_PRODUCT] }}</label>
                                         @endif
                                         @if ($data->gosi_inspect == null || $data->gosi_inspect->is_inspect == InspectConstant::IS_INSPECT_N)
-                                            <br>
-                                            <span class="text-danger">{{ InspectConstant::INSPECT_LIST[InspectConstant::INSPECT_NOTICE] }}</span>
+                                            <label class="text-danger">{{ InspectConstant::INSPECT_LIST[InspectConstant::INSPECT_NOTICE] }}</label>
                                         @endif
                                     </td>
                                     <td class="text-center">
