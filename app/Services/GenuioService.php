e49798ff7d4f1124cd2980e76cca43efe1cb7860
junyeob kim <junyeob.kim@onch3.co.kr>
2024-05-28 09:18:28 +0900

    ge 코드 수정

--- app/Services/GenuioService.php
+++ app/Services/GenuioService.php
@@ -1199,10 +1199,10 @@ class GenuioService extends TransApiAbstract
     {
         $returnMsg = $this->returnMsg;
 
-        $debugLog = [
-            "params" => $params
-        ];
-        debug_log(json_encode($debugLog, JSON_UNESCAPED_UNICODE), "genuio/channelImgTrans", "channelImgTrans");
+        // $debugLog = [
+        //     "params" => $params
+        // ];
+        // debug_log(json_encode($debugLog, JSON_UNESCAPED_UNICODE), "genuio/channelImgTrans", "channelImgTrans");
 
         try {
             $jobId  = (int)$params["jobId"];
