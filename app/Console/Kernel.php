64aa2a1ddd78e02accea68912f12005dc61c8f69
kimjunyeob <devtoproduction@gmail.com>
2024-05-29 21:57:30 +0900

    배치시간 수정

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends ConsoleKernel
             $schedule->command("miss_product_re_collect --wversion=". WConstant::WAPP_W2)->cron("0 6 * * *")->description("정보부족 W2 상품 재수집")->withoutOverlapping()->runInBackground();
 
             $schedule->command("update_attribute")->cron("0 0 * * *")->description("정보고시 관리(제외, 항목명 수정)")->withoutOverlapping()->runInBackground();
-            $schedule->command("update_forbidden_word")->cron("0 30 * * *")->description("금칙어 사전 적용(상품, 정보고시 항목값)")->withoutOverlapping()->runInBackground();
+            $schedule->command("update_forbidden_word")->cron("30 0 * * *")->description("금칙어 사전 적용(상품, 정보고시 항목값)")->withoutOverlapping()->runInBackground();
             $schedule->command("update_weight_delivery")->cron("0 1 * * *")->description("중량별 배송비 적용")->withoutOverlapping()->runInBackground();
 
             /** 이지셀 */
