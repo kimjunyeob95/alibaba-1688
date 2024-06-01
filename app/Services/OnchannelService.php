01bd843d0bff5d2a9b8dc7979611d07ced25cbd3
kimjunyeob <devtoproduction@gmail.com>
2024-06-01 19:47:41 +0900

    feat: 온채널 상품현황 수정

--- app/Services/OnchannelService.php
+++ app/Services/OnchannelService.php
@@ -124,19 +124,21 @@ class OnchannelService
             }
         }
 
-        $successCnt = OnchannelProductLog::where("regist_success", MallConstant::REGIST_SUCCESS)->count();
+        $countBuilder = ProductData::leftJoin("onchannel_product_logs as b", "product_datas.offer_id", "=", "b.offer_id")
+            ->whereIn("product_datas.w_type", [ WConstant::WAPP_W1, WConstant::WAPP_W2])
+            ->where("product_datas.mapping_status", ProductConstant::MAPPING_STATUS_Y)
+            ->where("product_datas.status", "!=", ProductConstant::PRD_STATUS_MISS);
 
-        $failBuilder = ProductData::leftJoin("onchannel_product_logs as b", "product_datas.offer_id", "=", "b.offer_id")
-        ->whereIn("product_datas.w_type", [ WConstant::WAPP_W1, WConstant::WAPP_W2])
-        ->where("product_datas.mapping_status", ProductConstant::MAPPING_STATUS_Y)
-        ->where("product_datas.status", "!=", ProductConstant::PRD_STATUS_MISS);
-        $failCnt = $failBuilder->where(function($query){
+        $successCnt = (clone $countBuilder)->where("regist_success", MallConstant::REGIST_SUCCESS)->count();
+
+        $failCnt = (clone $countBuilder)->where(function($query){
             $query->where("b.regist_success", MallConstant::REGIST_FAIL)
                 ->orWhereNull("b.regist_success");
         })->count();
 
-        $errorCnt   = OnchannelProductLog::where("regist_success", MallConstant::REGIST_ERROR)->count();
-        $lists      = $prdBuilder->paginate($pageSize)->appends($params);
+        $errorCnt = (clone $countBuilder)->where("regist_success", MallConstant::REGIST_ERROR)->count();
+
+        $lists = $prdBuilder->paginate($pageSize)->appends($params);
 
         return [
             "paginator"      => $lists,
