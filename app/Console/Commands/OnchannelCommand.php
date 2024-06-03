a6b475c0b4a69cbd1e15d210c48ee8a1e255d821
junyeob kim <junyeob.kim@onch3.co.kr>
2024-06-03 18:15:05 +0900

    온채널 자동 전송 조건 수정

--- app/Console/Commands/OnchannelCommand.php
+++ app/Console/Commands/OnchannelCommand.php
@@ -39,15 +39,11 @@ class OnchannelCommand extends Command
                     "product_datas.offer_id",
                 ])
                 ->leftJoin("onchannel_product_logs as b", "product_datas.offer_id", "=", "b.offer_id")
-                ->whereIn("product_datas.w_type", [ WConstant::WAPP_W1, WConstant::WAPP_W2 ])
                 ->where("product_datas.mapping_status", ProductConstant::MAPPING_STATUS_Y)
                 ->where("product_datas.status", "!=", ProductConstant::PRD_STATUS_MISS);
 
                 $prdBuilder->where(function($query){
-                    $query->where("b.regist_success", MallConstant::REGIST_FAIL)
-                        ->orWhere("b.message", "온채널 통신 에러")
-                        ->orWhere("b.message", "Empty options")
-                        ->orWhere("b.message", "상품채널(을)를 입력해주세요.")
+                    $query->where("b.regist_success", "!=", MallConstant::REGIST_SUCCESS)
                         ->orWhereNull("b.regist_success");
                 });
 
