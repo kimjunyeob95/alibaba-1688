abea5f4482dcdbdd7c4a5322aa77b89b2d7f1f54
jhjang <jungho.jang@sellerhub.co.kr>
2024-06-11 08:38:02 +0900

    refactor: add log queue callback

--- app/Traits/MallImageTrait.php
+++ app/Traits/MallImageTrait.php
@@ -2,20 +2,21 @@
 
 namespace App\Traits;
 
-use App\Abstracts\TransApiAbstract;
 use Exception;
+use App\Abstracts\TransApiAbstract;
+use Illuminate\Support\Facades\Log;
 
 trait MallImageTrait
 {
     protected array $returnMsg;
     protected string $channel;
     protected TransApiAbstract $transApiImageAbstract;
-    
+
     public function __construct()
     {
         $this->returnMsg = helpers_fail_message();
     }
-    
+
     public function initImageTrait(string $channel, TransApiAbstract $transApiImageAbstract): void
     {
         $this->channel               = $channel;
@@ -55,6 +56,9 @@ trait MallImageTrait
     public function imgTrans($params): array
     {
         $returnMsg = $this->returnMsg;
+
+        Log::info("=== SAI Callback images ===");
+        Log::info($params);
 
         try {
             $result = $this->transApiImageAbstract->channelImgTrans($this->channel, $params);
