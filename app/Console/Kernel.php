4e2ec8de20b61b2e19ac983378529a1c654a7561
junyeob kim <junyeob.kim@onch3.co.kr>
2024-06-13 10:20:27 +0900

    온채널 일반상품 배치 등록

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -70,8 +70,8 @@ class Kernel extends ConsoleKernel
             /** 이지셀 */
             $schedule->command("easy_sell_command --func=sendModiProduct")->cron("*/5 * * * *")->description("이지셀 수정 된 상품 전송")->withoutOverlapping()->runInBackground();
 
-            /** 온채널 신규 상품 등록 */
-            // $schedule->command("onchannel_command --func=newProductRegist")->cron("0 9,18 * * *")->description("온채널 신규 상품 등록")->withoutOverlapping()->runInBackground();
+            /** 온채널 신규 일반상품 등록 */
+            $schedule->command("onchannel_command --func=newProductRegist")->cron("0 9,18 * * *")->description("온채널 신규 상품 등록")->withoutOverlapping()->runInBackground();
 
             // $schedule->command("test --filter testAllProductReCollectW1")->description("모든 상품 W1 재수집")->withoutOverlapping()->runInBackground();
         }
