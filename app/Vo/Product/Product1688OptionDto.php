ca2fb26708fcc61e82416966d0b010130cc1fbbc
junyeob kim <junyeob.kim@onch3.co.kr>
2024-06-13 12:00:12 +0900

    옵션별 가격 저장

--- app/Vo/Product/Product1688OptionDto.php
+++ app/Vo/Product/Product1688OptionDto.php
@@ -19,8 +19,12 @@ class Product1688OptionDto extends Vo
     protected float $price_1688_option = 0.0;
     protected float $option_price      = 0.0;
     protected float $md_price          = 0.0;
+    protected float $onch_price        = 0.0;
+    protected float $cus_price         = 0.0;
+    protected float $recom_cus_price   = 0.0;
     protected int $amount_on_sale      = 0;
     protected string $cargo_number     = "";
+    protected float $exchange_rate     = 0.0;
     protected float $width             = 0.0;
     protected float $length            = 0.0;
     protected float $height            = 0.0;
@@ -45,5 +49,21 @@ class Product1688OptionDto extends Vo
         $this->length            = $data["length"];
         $this->height            = $data["height"];
         $this->weight            = $data["weight"];
+
+        $this->oc_bind();
+    }
+
+    public function oc_bind(): void
+    {
+        $this->option_price = round( $this->price_1688 * $this->exchange_rate, -1);  // 1의 자리 반올림
+
+        $option_price_sum = (int)intval($this->option_price) + intval($this->option_price * env("OPTION_PRICE_RATE", 0.12));
+        $option_price_cal = round($option_price_sum / 10) * 10;
+        $this->onch_price = $option_price_cal;
+
+        $recom_cus_price_sum = (int)intval($this->option_price) + intval($this->option_price * env("RECOM_CUS_PRICE_RATE", 0.45));
+        $recom_cus_price_cal = round($recom_cus_price_sum / 10) * 10;
+        $this->cus_price       = $recom_cus_price_cal;
+        $this->recom_cus_price = $recom_cus_price_cal;
     }
 }
