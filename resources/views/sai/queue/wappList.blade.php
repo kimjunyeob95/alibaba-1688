39ad8906005bcb15c046e05a284080e400812b25
junyeob kim <junyeob.kim@onch3.co.kr>
2024-06-21 10:51:12 +0900

    링크 이동 추가

--- resources/views/sai/queue/wappList.blade.php
+++ resources/views/sai/queue/wappList.blade.php
@@ -146,7 +146,7 @@
                                         {{ $data->id }}
                                     </td>
                                     <td>
-                                        {{ $data->offer_id }}
+                                        <a href="/product/{{ $data->offer_id }}">{{ $data->offer_id }}</a>
                                     </td>
                                     <td>
                                         {{ GenuioConstant::IMG_TRANS_TYPE[$data->send_type] }}
