d6b8a997f92692737e1b1db08d7400e01f27de2b
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-07-02 10:15:45 +0900

    상품의 카테고리 기준으로 채널 별 선택 전송이 가능하도록 설정

--- app/Packages/EasySell.php
+++ app/Packages/EasySell.php
@@ -12,6 +12,7 @@ use App\Constants\MallConstant;
 use App\Constants\MallErrorMessageConstant;
 use App\Constants\ProductConstant;
 use App\Models\CategoryMapping;
+use App\Models\ChannelCategoryRegistData;
 use App\Models\EasysellProductDetailLog;
 use App\Models\EasysellProductLog;
 use App\Models\ProductData;
@@ -67,10 +68,10 @@ class EasySell extends MallApiAbstract
                 try{
                     switch($w_type){
                         case EasySellConstant::TYPE_W :
-                            $account      = EasySellConstant::USER_ID_W;
+                            $account = EasySellConstant::USER_ID_W;
                             break;
                         case EasySellConstant::TYPE_DROPHUB:
-                            $account      = EasySellConstant::USER_ID_DROPHUB;
+                            $account = EasySellConstant::USER_ID_DROPHUB;
                             break;
                         default :
                             throw new Exception(MallErrorMessageConstant::getNotHaveErrorMessage("TYPE"));
@@ -103,6 +104,18 @@ class EasySell extends MallApiAbstract
                     if( $prdObj == null ){
                         throw new Exception(MallErrorMessageConstant::getNotHaveErrorMessage("PRODUCT"));
                     }
+
+                    $channelCnt = ChannelCategoryRegistData::where([
+                        "channel"     => $this->channel,
+                        "send_type"   => $w_type,
+                        "category_id" => $prdObj->category_id,
+                        "is_regist"   => MallConstant::REGIST_Y,
+                    ])->count();
+
+                    if( $channelCnt == 0 ){
+                        throw new Exception(MallErrorMessageConstant::getFitErrorMessage("CATEGORY_REGIST"));
+                    }
+
                     if( $prdObj->trans_status != ProductConstant::IMG_TRANS_Y ){
                         throw new Exception(MallErrorMessageConstant::getFitErrorMessage("NOT_TRANS_IMG"));
                     }
@@ -248,6 +261,18 @@ class EasySell extends MallApiAbstract
                 if( $prdObj == null ){
                     throw new Exception(MallErrorMessageConstant::getNotHaveErrorMessage("PRODUCT"));
                 }
+
+                $channelCnt = ChannelCategoryRegistData::where([
+                    "channel"     => $this->channel,
+                    "send_type"   => $type,
+                    "category_id" => $prdObj->category_id,
+                    "is_regist"   => MallConstant::REGIST_Y,
+                ])->count();
+
+                if( $channelCnt == 0 ){
+                    throw new Exception(MallErrorMessageConstant::getFitErrorMessage("CATEGORY_REGIST"));
+                }
+
                 if( $prdObj->trans_status != ProductConstant::IMG_TRANS_Y ){
                     throw new Exception(MallErrorMessageConstant::getFitErrorMessage("NOT_TRANS_IMG"));
                 }
