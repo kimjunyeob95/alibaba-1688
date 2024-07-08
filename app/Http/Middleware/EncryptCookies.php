cef7f57b4fa08d21490e1a87f73b1339d59cc928
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-07-08 10:41:17 +0900

    쿠키모듈 수정

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -14,4 +14,47 @@ class EncryptCookies extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * Decrypt the given cookie and return the value.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @return mixed
+     */
+    protected function decryptCookie($name, $value)
+    {
+        // 쿠키의 도메인을 가져옵니다.
+        $cookieDomain = $this->getCookieDomain($name);
+
+        // 현재 애플리케이션의 도메인과 비교합니다.
+        if ($cookieDomain !== config('session.domain')) {
+            // 도메인이 다르면 복호화를 하지 않습니다.
+            return $value;
+        }
+
+        // 도메인이 같으면 정상적으로 복호화합니다.
+        return parent::decryptCookie($name, $value);
+    }
+
+    /**
+     * Get the domain of the given cookie.
+     *
+     * @param  string  $name
+     * @return string|null
+     */
+    protected function getCookieDomain($name)
+    {
+        // 여기서 쿠키의 도메인을 추출하는 로직을 추가합니다.
+        // 예제로서 현재 null을 반환합니다.
+        // 실제 구현에서는 $_COOKIE 또는 다른 방법으로 도메인을 추출할 수 있습니다.
+        if (isset($_COOKIE[$name])) {
+            // 도메인 추출 로직을 추가합니다.
+            if( isset($_SERVER['HTTP_REFERER']) ){
+                return parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+            }
+        }
+
+        return null;
+    }
 }
