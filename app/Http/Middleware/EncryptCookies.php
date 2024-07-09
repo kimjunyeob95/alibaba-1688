b4011b20943cfeee4e49e027a6cc9f1c09f0376d
kimjunyeob <devtoproduction@gmail.com>
2024-07-10 00:40:07 +0900

    쿠키 복호화 수정

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -24,35 +24,13 @@ class EncryptCookies extends Middleware
      */
     protected function decryptCookie($name, $value)
     {
-        // 쿠키의 도메인을 가져옵니다.
-        $cookieDomain = $this->getCookieDomain($name);
+        $decryptList = ['1688_session', 'XSRF-TOKEN'];
 
-        // 현재 애플리케이션의 도메인과 비교합니다.
-        if ($cookieDomain !== config('session.domain')) {
-            // 도메인이 다르면 복호화를 하지 않습니다.
-            return $value;
+        /**
+         * 다른 서브 도메인의 쿠키를 복호화하면 whatap에 에러로그가 발생해 특정 쿠키만 복호화
+         */
+        if( in_array($name, $decryptList) ){
+            return parent::decryptCookie($name, $value);
         }
-
-        // 도메인이 같으면 정상적으로 복호화합니다.
-        return parent::decryptCookie($name, $value);
-    }
-
-    /**
-     * Get the domain of the given cookie.
-     *
-     * @param  string  $name
-     * @return string|null
-     */
-    protected function getCookieDomain($name)
-    {
-        // 여기서 쿠키의 도메인을 추출하는 로직을 추가
-        if (isset($_COOKIE[$name])) {
-            // 도메인 추출 로직을 추가합니다.
-            if( isset($_SERVER['HTTP_REFERER']) ){
-                return parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
-            }
-        }
-
-        return null;
     }
 }
