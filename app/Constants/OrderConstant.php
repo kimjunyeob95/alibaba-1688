0fd95601122bcaf76f7b3b61172c59a9f89df61e
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-07-09 16:07:29 +0900

    작업중...

--- app/Constants/OrderConstant.php
+++ app/Constants/OrderConstant.php
@@ -5,22 +5,61 @@ namespace App\Constants;
 
 class OrderConstant
 {
-    // 주문 상태
-    public const wait_pay         = "waitbuyerpay";
-    public const wait_delivery    = "waitellerend";
-    public const wait_receive     = "waitbuyerreceive";
-    public const receive_done     = "confirm_goods";
-    public const order_success    = "success";
-    public const order_cancel     = "cancel";
-    public const order_terminated = "terminated";
+    /** 주문상태 */
+    public const WAIT_PAY         = "waitbuyerpay";
+    public const WAIT_DELIVERY    = "waitellerend";
+    public const WAIT_RECEIVE     = "waitbuyerreceive";
+    public const RECEIVE_DONE     = "confirm_goods";
+    public const ORDER_SUCCESS    = "success";
+    public const ORDER_CANCEL     = "cancel";
+    public const ORDER_TERMINATED = "terminated";
+    public const ORDER_STATUS     = [
+        self::WAIT_PAY         => "결제대기(구매자 결제대기)",
+        self::WAIT_DELIVERY    => "결제완료(판매자 발송 대기)",
+        self::WAIT_RECEIVE     => "출고(구매자 수령대기)",
+        self::RECEIVE_DONE     => "배송완료(수령확인)",
+        self::ORDER_SUCCESS    => "거래완료",
+        self::ORDER_CANCEL     => "거래취소",
+        self::ORDER_TERMINATED => "거래종료(기타상태)"
+    ];
 
-    public const order_status_list = [
-        self::wait_pay         => "결제 대기",
-        self::wait_delivery    => "발송 대기",
-        self::wait_receive     => "배송중",
-        self::receive_done     => "배송완료",
-        self::order_success    => "주문 성공",
-        self::order_cancel     => "주문 취소",
-        self::order_terminated => "거래종료(기타상태)"
+    /** 주문 취소 사유 */
+    public const CLOSE_REASON_BUYER_CANCEL            = "buyerCancel";
+    public const CLOSE_REASON_BUYER_SELLER_GOODS_LACK = "sellerGoodsLack";
+    public const CLOSE_REASON_BUYER_OTHER             = "other";
+    public const CLOSE_REASON                         = [
+        self::CLOSE_REASON_BUYER_CANCEL            => "구매자 취소",
+        self::CLOSE_REASON_BUYER_SELLER_GOODS_LACK => "재고부족",
+        self::CLOSE_REASON_BUYER_OTHER             => "기타",
+    ];
+
+    /** 거래 종료 사유 */
+    public const CLOSE_ORDER_CLOSE_TRADE_BY_SELLER  = "CLOSE_TRADE_BY_SELLER";
+    public const CLOSE_ORDER_CLOSE_TRADE_BY_BUYER   = "CLOSE_TRADE_BY_BUYER";
+    public const CLOSE_ORDER_CLOSE_TRADE_BY_BOPS    = "CLOSE_TRADE_BY_BOPS";
+    public const CLOSE_ORDER_CLOSE_TRADE_BY_SYSTEM  = "CLOSE_TRADE_BY_SYSTEM";
+    public const CLOSE_ORDER_CLOSE_TRADE_BY_CREADIT = "CLOSE_TRADE_BY_CREADIT";
+    public const CLOSE_ORDER                        = [
+        self::CLOSE_ORDER_CLOSE_TRADE_BY_SELLER  => "판매자종료",
+        self::CLOSE_ORDER_CLOSE_TRADE_BY_BUYER   => "구매자종료",
+        self::CLOSE_ORDER_CLOSE_TRADE_BY_BOPS    => "백그라운드에서 종료",
+        self::CLOSE_ORDER_CLOSE_TRADE_BY_SYSTEM  => "시스템(시간초과)",
+        self::CLOSE_ORDER_CLOSE_TRADE_BY_CREADIT => "기타",
+    ];
+
+    /** 환불 사유 */
+    public const REFUND_WAITSELLERAGREE   = "waitselleragree";
+    public const REFUND_WAITBUYERMODIFY   = "waitbuyermodify";
+    public const REFUND_WAITBUYERSEND     = "waitbuyersend";
+    public const REFUND_WAITSELLERRECEIVE = "waitsellerreceive";
+    public const REFUND_REFUNDSUCCESS     = "refundsuccess";
+    public const REFUND_REFUNDCLOSE       = "refundclose";
+    public const REFUND_REASON            = [
+        self::REFUND_WAITSELLERAGREE   => "판매자 동의대기",
+        self::REFUND_WAITBUYERMODIFY   => "구매자 수정대기",
+        self::REFUND_WAITBUYERSEND     => "구매자 발송대기",
+        self::REFUND_WAITSELLERRECEIVE => "판매자 수령대기",
+        self::REFUND_REFUNDSUCCESS     => "환불완료",
+        self::REFUND_REFUNDCLOSE       => "환불실패",
     ];
 }
--- app/Vo/Order/OrderDto.php
+++ app/Vo/Order/OrderDto.php
@@ -8,8 +8,6 @@ class OrderDto extends Vo
 {
     protected string $order_id               = "";
     protected int $offer_id                  = 0;
-    protected string $channel_order_id       = "";
-    protected string $channel                = "";
     protected string $buyer_name             = "";
     protected string $buyer_clearance_number = "";
     protected string $buyer_number           = "";
@@ -22,18 +20,37 @@ class OrderDto extends Vo
 
     public function bind(mixed $data): void
     {
-        $this->order_id               = $data["order_id"];
-        $this->offer_id               = $data["offer_id"];
-        $this->channel_order_id       = $data["channel_order_id"];
+        $baseInfo = $data["baseInfo"] ?? [];
+
+        if( !empty($baseInfo) ){
+            $this->order_id            = $data["id"];
+            $this->status              = $data["status"];
+            $this->all_delivered_time  = $data["allDeliveredTime"] ?? null;
+            $this->pay_time            = $data["payTime"] ?? null;
+            $this->discount            = $data["discount"] ?? 0;
+            $this->sum_product_payment = $data["sumProductPayment"] ?? 0;
+            $this->modify_time         = $data["modifyTime"] ?? null;
+            $this->close_reason        = $data["closeReason"] ?? "";
+            $this->complete_time       = $data["completeTime"] ?? null;
+            $this->close_operate_type  = $data["closeOperateType"] ?? "";
+            $this->total_amount        = $data["totalAmount"] ?? 0;
+            $this->seller_id           = $data["sellerID"] ?? "";
+            $this->shipping_fee        = $data["shippingFee"] ?? 0;
+            $this->refund              = $data["refund"] ?? 0;
+            $this->refund_payment      = $data["refundPayment"] ?? 0;
+            $this->refund_status       = $data["refundStatus"] ?? 0;
+        }
+
+        $this->offer_id               = $data["offerId"];
         $this->channel                = $data["channel"];
-        $this->buyer_name             = $data["buyer_name"];
-        $this->buyer_clearance_number = $data["buyer_clearance_number"];
-        $this->buyer_number           = $data["buyer_number"];
-        $this->buyer_phone            = $data["buyer_phone"];
-        $this->buyer_zipcode          = $data["buyer_zipcode"];
-        $this->buyer_address          = $data["buyer_address"];
-        $this->buyer_memo             = $data["buyer_memo"];
-        $this->total_quantity         = $data["total_quantity"];
-        $this->total_price            = $data["total_price"];
+        // $this->buyer_name             = $data["buyer_name"];
+        // $this->buyer_clearance_number = $data["buyer_clearance_number"];
+        // $this->buyer_number           = $data["buyer_number"];
+        // $this->buyer_phone            = $data["buyer_phone"];
+        // $this->buyer_zipcode          = $data["buyer_zipcode"];
+        // $this->buyer_address          = $data["buyer_address"];
+        // $this->buyer_memo             = $data["buyer_memo"];
+        // $this->total_quantity         = $data["total_quantity"];
+        // $this->total_price            = $data["total_price"];
     }
 }
