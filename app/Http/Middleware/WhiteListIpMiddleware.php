cfebef8afe73eec68112faf4a508236d6cff5a28
kimjunyeob <devtoproduction@gmail.com>
2024-07-17 10:20:04 +0900

    ip 체크 원복

--- app/Http/Middleware/WhiteListIpMiddleware.php
+++ app/Http/Middleware/WhiteListIpMiddleware.php
@@ -14,12 +14,14 @@ class WhiteListIpMiddleware
         if( $exceptRoute != "/api/w/1688/test" ){
             // .env에서 허용된 IP 목록을 가져옵니다.
             $allowedIps = explode(',', env('WHITELIST_IPS'));
-            $msg = "ip: ". $request->ip();
-            debug_log($msg, "wapp/checkIp", "checkIp");
+
+            // $msg = "ip: ". $request->ip();
+            // debug_log($msg, "wapp/checkIp", "checkIp");
+
             // 요청 IP가 허용 목록에 없으면 접근을 거부합니다.
-            // if (!in_array($request->ip(), $allowedIps)) {
-            //     abort(403, 'No White List Ip');
-            // }
+            if (!in_array($request->ip(), $allowedIps)) {
+                abort(403, 'No White List Ip');
+            }
         }
 
         return $next($request);
