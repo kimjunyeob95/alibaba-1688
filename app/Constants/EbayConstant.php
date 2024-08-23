505a20f5977bc4f1f4a573a85ef17b1ead3ff7d6
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-08-23 13:55:37 +0900

    feat: ebay 카테고리 조회 테스트 코드 작성

---
+++ app/Constants/EbayConstant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+class EbayConstant
+{
+    public const SANDBOX     = "SANDBOX";
+    public const PRODUCTION  = "PRODUCTION";
+    public const API_VERSION = "1173";
+ 
+}
