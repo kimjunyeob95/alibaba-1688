c021913056dcbb44968e3c1de067a8adb622a26f
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-09-05 10:07:32 +0900

    feat: WAPP: 채널 주문 데이터에 통관유형 및 운송방식의 추가 #17134

--- app/Services/Order/OrderW1.php
+++ app/Services/Order/OrderW1.php
@@ -593,6 +593,8 @@ class OrderW1 extends OrderAbstract
             $channelPrices        = $params["channelPrices"];
             $orderChannel         = trim($params["orderChannel"]);
             $channelOrderId       = trim($params["channelOrderId"]);
+            $clearanceType        = trim($params["clearanceType"]);
+            $shippingType         = trim($params["shippingType"]);
             $deliveryPrice        = (float)$params["deliveryPrice"];
             $buyerName            = trim($params["buyerName"]);
             $buyerClearanceNumber = trim($params["buyerClearanceNumber"]);
@@ -601,8 +603,6 @@ class OrderW1 extends OrderAbstract
             $buyerAddress         = trim($params["buyerAddress"]);
             $buyerZipcode         = trim($params["buyerZipcode"]);
             $buyerMemo            = trim($params["buyerMemo"]);
-            $clearanceType        = OrderConstant::CLEARANCE_SA_TYPE;
-            $shippingType         = OrderConstant::SHIPPING_OF_TYPE;
 
             $orderDetailResult = $this->getWOrder($orderId);
             if( $orderDetailResult["isSuccess"] == false || 
