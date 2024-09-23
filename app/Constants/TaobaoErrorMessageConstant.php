11c25a8adb00612192d3ee58e6eb9ef12a134407
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-09-23 12:50:19 +0900

    feat: taobao sdk 연동 및 api 통신 코드 작성

---
+++ app/Constants/TaobaoErrorMessageConstant.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Constants;
+
+class TaobaoErrorMessageConstant
+{
+    private static $defaultMsg         = "(을)를 입력해주세요.";
+    private static $defaultTypeMsg     = "의 타입형식이 올바르지 않습니다.";
+    private static $defaultHaveMsg     = "(은)는 이미 등록되어 있습니다.";
+    private static $defaultNotHaveMsg  = "Empty";
+    private static $defaultFitErrorMsg = "Error";
+
+    public const ERROR_MESSAGE_TOKEN_OBJ     = "token obj";
+    public const ERROR_MESSAGE_ACCESS_TOKEN  = "access_token";
+    public const ERROR_MESSAGE_REFRESH_TOKEN = "refresh_token";
+ 
+    public static function getErrorMessageNotDefault($constantName): string
+    {
+        $errorMessage = constant('self::ERROR_MESSAGE_' . $constantName);
+        return $errorMessage;
+    }
+
+    public static function getErrorMessage($constantName): string
+    {
+        $errorMessage = constant('self::ERROR_MESSAGE_' . $constantName);
+        return $errorMessage . self::$defaultMsg;
+    }
+
+    public static function getTypeErrorMessage($constantName): string
+    {
+        $errorMessage = constant('self::ERROR_MESSAGE_' . $constantName);
+        return $errorMessage . self::$defaultTypeMsg;
+    }
+
+    public static function getHaveErrorMessage(string $constantName): string
+    {
+        $errorMessage = constant('self::ERROR_MESSAGE_' . $constantName);
+        return $errorMessage . self::$defaultHaveMsg;
+    }
+
+    public static function getNotHaveErrorMessage(string $constantName): string
+    {
+        $errorMessage = constant('self::ERROR_MESSAGE_' . $constantName);
+        return self::$defaultNotHaveMsg . " " . $errorMessage;
+    }
+
+    public static function getFitErrorMessage(string $constantName): string
+    {
+        $errorMessage = constant('self::ERROR_MESSAGE_' . $constantName);
+        return self::$defaultFitErrorMsg . " " . $errorMessage;
+    }
+}
---
+++ app/Packages/Taobao/UrlConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Packages\Taobao;
+
+class UrlConstants
+{
+	static $api_gateway_url_tw    = "https://api.taobao.global/rest";
+	static $api_authorization_url = "https://auth.taobao.tw/rest";
+}
