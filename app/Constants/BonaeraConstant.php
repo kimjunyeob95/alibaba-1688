74817613e7c43d386e99b40d4ff604e8b523aee3
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-09-27 14:46:54 +0900

    feat: 그룹 신청서 상태 정의 #15698

--- app/Constants/BonaeraConstant.php
+++ app/Constants/BonaeraConstant.php
@@ -26,24 +26,37 @@ class BonaeraConstant
     ];
 
     /** 그룹 신청서 상태 */
-    public const GROUP_STATUS_301   = "301";
-    public const GROUP_STATUS_302   = "302";
-    public const GROUP_STATUS_303   = "303";
-    public const GROUP_STATUS_304   = "304";
-    public const GROUP_STATUS_305   = "305";
-    public const GROUP_STATUS_306   = "306";
-    public const GROUP_STATUS_307   = "307";
-    public const GROUP_STATUS__PEKI = "_peki";
-    public const GROUP_STATUS_300   = "300";
-    public const GROUP_STATUS       = [
-        self::GROUP_STATUS_301   => "입고대기",
-        self::GROUP_STATUS_302   => "입고완료",
-        self::GROUP_STATUS_303   => "무게측정",
-        self::GROUP_STATUS_304   => "결제대기",
-        self::GROUP_STATUS_305   => "결제확인중",
-        self::GROUP_STATUS_306   => "출고준비",
-        self::GROUP_STATUS_307   => "출고완료",
-        self::GROUP_STATUS__PEKI => "입고대기 or 입고완료의 폐기",
-        self::GROUP_STATUS_300   => "무게측정단계의 폐기",
+    public const GROUP_STATUS_301      = "301";
+    public const GROUP_STATUS_302      = "302";
+    public const GROUP_STATUS_303      = "303";
+    public const GROUP_STATUS_304      = "304";
+    public const GROUP_STATUS_305      = "305";
+    public const GROUP_STATUS_306      = "306";
+    public const GROUP_STATUS_307      = "307";
+    public const GROUP_STATUS_301_PEKI = "301_peki";
+    public const GROUP_STATUS_302_PEKI = "302_peki";
+    public const GROUP_STATUS_303_PEKI = "303_peki";
+    public const GROUP_STATUS_304_PEKI = "304_peki";
+    public const GROUP_STATUS_305_PEKI = "305_peki";
+    public const GROUP_STATUS_306_PEKI = "306_peki";
+    public const GROUP_STATUS_307_PEKI = "307_peki";
+    public const GROUP_STATUS_300      = "300";
+    public const GROUP_STATUS          = [
+        self::GROUP_STATUS_301      => "입고대기",
+        self::GROUP_STATUS_302      => "입고완료",
+        self::GROUP_STATUS_303      => "무게측정",
+        self::GROUP_STATUS_304      => "결제대기",
+        self::GROUP_STATUS_305      => "결제확인중",
+        self::GROUP_STATUS_306      => "출고준비",
+        self::GROUP_STATUS_307      => "출고완료",
+        self::GROUP_STATUS_300      => "폐기",
+        self::GROUP_STATUS_301_PEKI => self::GROUP_STATUS_301 . "에서 폐기",
+        self::GROUP_STATUS_302_PEKI => self::GROUP_STATUS_302 . "에서 폐기",
+        self::GROUP_STATUS_303_PEKI => self::GROUP_STATUS_303 . "에서 폐기",
+        self::GROUP_STATUS_304_PEKI => self::GROUP_STATUS_304 . "에서 폐기",
+        self::GROUP_STATUS_305_PEKI => self::GROUP_STATUS_305 . "에서 폐기",
+        self::GROUP_STATUS_306_PEKI => self::GROUP_STATUS_306 . "에서 폐기",
+        self::GROUP_STATUS_307_PEKI => self::GROUP_STATUS_307 . "에서 폐기",
+
     ];
 }
