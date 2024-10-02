1187ccb9f2d2141474e845af7c44c72007bcb26d
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-10-02 09:56:59 +0900

    fix: 문법 오류 수정

--- resources/views/easysell/prdList.blade.php
+++ resources/views/easysell/prdList.blade.php
@@ -299,7 +299,7 @@ input[name='channelCategory']{
                                                 $option = $data->options[0];
 
                                                 //배송비 설정
-                                                $$delivery_price = getWeightDelivery($data->weight)["shipping_price"];
+                                                $delivery_price = getWeightDelivery($data->weight)["shipping_price"];
 
                                                 if($send_type == EasySellConstant::TYPE_W){
                                                     $price = calcEasySellSalePrice($option->price_1688, $option->md_price, $delivery_price, "static", EasySellConstant::TYPE_W);
