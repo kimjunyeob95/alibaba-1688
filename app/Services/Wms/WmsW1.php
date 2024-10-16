7a7fcf997c2f3f52d116d29257cbf4f6a1d2c3b6
kimjunyeob <devtoproduction@gmail.com>
2024-10-16 10:39:59 +0900

    feat: 출고 신청 관리 페이지의 수정 #17938

--- app/Services/Wms/WmsW1.php
+++ app/Services/Wms/WmsW1.php
@@ -434,12 +434,11 @@ class WmsW1 extends WmsAbstract
             ->with(["details.option", "boneara_in_base.in_options", "order.product"])
             ->join("order_base_datas as obd", "order_channel_datas.order_id", "=", "obd.order_id")
             ->leftJoin("bonaera_out_base_datas as bobd", "order_channel_datas.order_id", "=", "bobd.order_id")
+            ->leftJoin("bonaera_in_base_datas as bibd", "order_channel_datas.order_id", "=", "bibd.order_id")
             ->join("bonaera_in_product_datas as bipd", "order_channel_datas.order_id", "=", "bipd.order_id")
             ->leftJoin("bonaera_out_fail_datas as bofd", "order_channel_datas.order_id", "=", "bofd.order_id")
             ->groupBy("order_channel_datas.order_id")
             ->orderBy("order_channel_datas." . $sortArr[0], $sortArr[1]);
-
-            $builder->where("bipd.status", BonaeraConstant::WAREHOUSE_STATUS_RECEIVED);
 
             if( !empty($status) ){
                 if( $status == WmsConstant::OUT_SIGN_STATUS_SUCCESS ){
@@ -452,8 +451,8 @@ class WmsW1 extends WmsAbstract
                     ->whereNull("bofd.msg");
                 }
             }
-            if( !empty($status) ){
-                $builder->where("bipd.status", $status);
+            if( !empty($inStatus) ){
+                $builder->where("bipd.status", $inStatus);
             }
             if( !empty($timeCls) ){
                 if( $timeCls == "create" ){
@@ -487,7 +486,10 @@ class WmsW1 extends WmsAbstract
                 } else if( $search_cls == WmsConstant::OUT_SIGN_SEARCH_TYPE_CHANNEL_ORDER_ID ){
                     $builder->whereIn("order_channel_datas.channel_order_id", $keyword);
                 } else if( $search_cls == WmsConstant::OUT_SIGN_SEARCH_TYPE_STOCK_NO ){
-                    $builder->whereIn("bobd.stock_no", $keyword);
+                    $builder->where(function($qry) use($keyword) {
+                        $qry->whereIn("bobd.stock_no", $keyword)
+                        ->orWhereIn("bibd.stock_no", $keyword);
+                    });
                 } else if( $search_cls == WmsConstant::OUT_SIGN_SEARCH_TYPE_SH_NO ){
                     $builder->whereIn("bobd.sh_no", $keyword);
                 } else if( $search_cls == WmsConstant::OUT_SIGN_SEARCH_TYPE_GROUP_NO ){
@@ -496,8 +498,8 @@ class WmsW1 extends WmsAbstract
             }
 
             $lists     = $builder->paginate($pageSize)->appends($params);
-            // dd($lists->toArray());
             $returnMsg = helpers_success_message($lists);
+            // dd($lists->toArray());
         } catch (Exception $e) {
             $returnMsg = helpers_fail_message($e->getMessage());
         }
