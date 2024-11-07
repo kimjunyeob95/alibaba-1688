067a3fe972e2c6b0ebef06f384741b65014268f7
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-11-07 11:50:37 +0900

    queue 캐싱 test

--- app/Services/Order/OrderW1.php
+++ app/Services/Order/OrderW1.php
@@ -828,7 +828,7 @@ class OrderW1 extends OrderAbstract
     public function queueTest(): void
     {
         $params = [
-            "version" => 2
+            "version" => 3
         ];
         debug_log(json_encode($params, JSON_UNESCAPED_UNICODE), "1688/queueTest", "queueTest");
     }
