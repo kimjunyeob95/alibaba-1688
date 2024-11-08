69ca0c485583bfcdccc9d8a1731ea959bfb4f31e
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-11-08 11:52:47 +0900

    feat: 입고완료일자 로직 수정

--- app/Services/Wms/WmsW1.php
+++ app/Services/Wms/WmsW1.php
@@ -338,14 +338,14 @@ class WmsW1 extends WmsAbstract
 
             /** 재고현황 조회 */
             $res = $this->bonaera->getStockList($inBaseObj->stock_no);
-
+            // dd($res);
             if( $res["isSuccess"] === true && isset($res["data"]["appCode"]) && isset($res["data"]["appitemList"]) ) {
                 $stockCode       = $res["data"]["appCode"];
                 $lastCompletedAt = null;
                 foreach ($res["data"]["appitemList"] as $item) {
                     if( !empty($item["itemIndate"]) ){
                         $currentDate = Carbon::parse($item["itemIndate"]);
-                        if ( $currentDate->gt($lastCompletedAt)) { // 가장 최근 날짜로 할당
+                        if ( $lastCompletedAt === null || $currentDate->gt($lastCompletedAt) ) { // 가장 최근 날짜로 할당
                             $lastCompletedAt = $currentDate;
                         }
                     }
