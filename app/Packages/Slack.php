4039fcdc783b894659a6f2b9c71b717fe7411ac9
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-11-18 17:27:39 +0900

    feat: wms : webhook 메세지 발신 웹푹 테스트 코드 작성 #18659

---
+++ app/Packages/Slack.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Packages;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Throwable;
+
+class Slack
+{
+    private array $returnMsg;
+    private string $webhookUrl;
+    private Client $client;
+
+    public function __construct(string $webhookUrl)
+    {
+        $this->returnMsg  = helpers_fail_message();
+        $this->webhookUrl = $webhookUrl;
+        $this->client     = new Client();
+    }
+
+    public function sendMessage(string $message): array
+    {
+        $returnMsg = $this->returnMsg;
+
+        try {
+            $response = $this->client->post($this->webhookUrl, [
+                'json' => [
+                    'text' => $message
+                ]
+            ]);
+
+            if ($response->getStatusCode() === 200) {
+                $returnMsg = helpers_success_message();
+            } else {
+                $body         = json_decode($response->getBody()->getContents(), true);
+                $errorMessage = $body['error'] ?? '알 수 없는 에러가 발생했습니다';
+                throw new GuzzleException($errorMessage);
+            }
+        } catch (GuzzleException $e) {
+            $returnMsg = helpers_fail_message('Slack error: ' . $e->getMessage());
+        } catch (Throwable $e) {
+            $returnMsg = helpers_fail_message('Throwable error: ' . $e->getMessage());
+        }
+        return $returnMsg;
+    }
+    
+}
