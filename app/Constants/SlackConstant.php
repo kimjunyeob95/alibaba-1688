e15d7d96944753a60c09d6efda75f64baac4bf98
kimjunyeob <devtoproduction@gmail.com>
2024-11-26 18:48:09 +0900

    wms_정보알림 슬랙 url 명 수정

--- app/Constants/SlackConstant.php
+++ app/Constants/SlackConstant.php
@@ -5,6 +5,6 @@ namespace App\Constants;
 
 class SlackConstant
 {
-    /** 입고 관련 */
-    public const BONAERA_IN_STATUS = "https://hooks.slack.com/services/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";
+    /** 9999_wms_정보알림 */
+    public const WMS_INFO_SLACK = "https://hooks.slack.com/services/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";
 }
