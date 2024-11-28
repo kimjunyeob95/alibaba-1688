a05ff51f623f099beb6012f64454f52eb28fd51c
junyeob.kim <junyeob.kim@onch3.co.kr>
2024-11-28 12:39:11 +0900

    feat: slack 토큰 env로 수정

--- app/Constants/SlackConstant.php
+++ app/Constants/SlackConstant.php
@@ -6,5 +6,8 @@ namespace App\Constants;
 class SlackConstant
 {
     /** 9999_wms_정보알림 */
-    public const WMS_INFO_SLACK = "https://hooks.slack.com/services/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";
+    public static function WMS_INFO_SLACK(): string
+    {
+        return env('WMS_INFO_SLACK');
+    }
 }
